d5cfe6e2e860e556960646b1c96719bf4730b971
BurhanuddinRaja <mails4burhan@gmail.com>
2022-09-13 16:59:24 +0530

    Searching Question added

---
+++ Algorithms/Searching/Easy/src/com/Burhan/Index_of_Last_Occurence.java
@@ -0,0 +1,32 @@
+package com.Burhan;
+
+public class Index_of_Last_Occurence {
+    public static void main(String[] args) {
+        int[] arr = {4, 9, 10, 20, 28, 36, 38, 38, 38, 43, 59, 62, 72};
+        int ans = bs(arr, 38, 0, arr.length-1);
+        System.out.println(ans);
+    }
+
+    static int bs(int[] arr, int x, int start, int end) {
+        if (start > end) {
+            return -1;
+        }
+
+        int mid = start + (end-start)/2;
+
+        if (arr[mid] < x) {
+            return bs(arr, x, mid+1, end);
+        }
+        else if (arr[mid] > x) {
+            return bs(arr, x, start, mid-1);
+        }
+        else {
+            if (mid ==0 || arr[mid+1] == arr[mid]) {
+                return bs(arr, x, mid+1, end);
+            }
+            else {
+                return mid;
+            }
+        }
+    }
+}
