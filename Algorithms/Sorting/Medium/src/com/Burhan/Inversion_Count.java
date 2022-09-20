4053bb6443e3f526685cd8f693ea782d1248b093
BurhanuddinRaja <mails4burhan@gmail.com>
2022-09-20 19:01:41 +0530

    updated

--- Algorithms/Sorting/Medium/src/com/Burhan/Inversion_Count.java
+++ Algorithms/Sorting/Medium/src/com/Burhan/Inversion_Count.java
@@ -1,16 +1,15 @@
 package com.Burhan;
 
-import java.util.Arrays;
-
 public class Inversion_Count {
     public static void main(String[] args) {
         // int[] arr = {1, 6, 2, 3};
         // int[] arr = {2, 4, 1, 3, 5};
         // int[] arr = {2, 3, 4, 5, 6};
         int[] arr = {1, 20, 6, 4, 5};
-        // int ans = inversionCount(arr);
-        int[] ans = mergeSort(arr);
-        System.out.println(Arrays.toString(ans));
+        int ans = inversionCount(arr);
+        // int[] ans = mergeSort(arr);
+        // System.out.println(Arrays.toString(ans));
+        System.out.println(ans);
     }
 
     static boolean isSorted(int[] arr) {
@@ -22,6 +21,7 @@ public class Inversion_Count {
         return true;
     }
 
+    // Naive
     static int inversionCount(int[] arr) {
         if (isSorted(arr)) {
             return 0;
@@ -51,55 +51,4 @@ public class Inversion_Count {
         arr[a] = arr[b];
         arr[b] = temp;
     }
-
-
-    static int[] mergeSort(int[] arr) {
-        if (arr.length == 1) {
-            return arr;
-        }
-
-        int half = arr.length/2;
-
-        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, half));
-        int[] right = mergeSort(Arrays.copyOfRange(arr, half, arr.length));
-
-        return merge(left, right);
-    }
-
-    static int[] merge(int[] left, int[] right) {
-        
-        int[] mix = new int[left.length + right.length];
-
-        int i = 0;
-        int j = 0;
-        int k = 0;
-
-        while (i < left.length && j < right.length) {
-            if (left[i] < right[j]){
-                mix[k] = left[i];
-                i++;
-            }
-            else {
-                mix[k] = right[j];
-                j++;
-            }
-            k++;
-        }
-
-        while (i < left.length) {
-            mix[k] = left[i];
-            i++;
-            k++;
-        }
-
-        while (j < right.length) {
-            mix[k] = right[j];
-            j++;
-            k++;
-        }
-
-        return mix;
-    }
-
-
 }
