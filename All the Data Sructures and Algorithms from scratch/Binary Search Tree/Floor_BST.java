451fd3aa6ce41829b9827b4f063b2447f4b16055
BurhanuddinRaja <mails4burhan@gmail.com>
2022-10-25 18:21:54 +0530

    Floor BST

---
+++ All the Data Sructures and Algorithms from scratch/Binary Search Tree/Floor_BST.java
@@ -0,0 +1,45 @@
+public class Floor_BST {
+
+    static class Node {
+        int key;
+        Node left;
+        Node right;
+
+        Node(int k) {
+            this.key = k;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(50);
+        root.left = new Node(30);
+        root.left.left = new Node(20);
+        root.left.right = new Node(40);
+
+        root.right = new Node(70);
+        root.right.left = new Node(60);
+        root.right.right = new Node(80);
+        root.right.left.left = new Node(56);
+        root.right.left.right = new Node(65);
+
+        Node ans = floorBST(root, 38);
+        System.out.println(ans.key);
+    }
+
+    static Node floorBST(Node root, int x) {
+        Node res = null;
+        while (root != null) {
+            if (root.key > x) {
+                root = root.left;
+            } else if (root.key < x) {
+                res = root;
+                root = root.right;
+            } else {
+                return root;
+            }
+        }
+
+        return res;
+    }
+
+}
