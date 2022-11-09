ea4dbb1644d623f33b48c5a5482023f4e6500d8a
BurhanuddinRaja <mails4burhan@gmail.com>
2022-11-09 09:25:48 +0530

    Replace with smaller value and Heapify

---
+++ All the Data Sructures and Algorithms from scratch/Heap/Replace_with_smaller_key.java
@@ -0,0 +1,79 @@
+public class Replace_with_smaller_key {
+    
+    static class Node {
+        int cap;
+        int size;
+        int[] arr;
+
+        Node (int c) {
+            this.cap = c;
+            arr = new int[cap];
+            size=0;
+        }
+
+        public void insert(int x) {
+            if (size == cap) {
+                return;
+            }
+            else {
+                arr[size] = x;
+                size++;
+                for (int i = size-1; i != 0 && arr[parent(i)] > arr[i]; i = parent(i)) {
+                    swap(arr, parent(i), i);
+                }
+            }
+        }
+
+        public void smallVal(int i, int x) {
+            if (i > size) {
+                return;
+            }
+            arr[i] = x;
+            while (i != 0 && arr[parent(i)] > arr[i]) {
+                swap(arr, parent(i), i);
+                i = parent(i);
+            }
+        }
+
+        
+
+        public void printHeap() {
+            for (int i = 0; i < size; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+
+        public void swap(int[] array, int a, int b) {
+            int temp = array[a];
+            array[a] = array[b];
+            array[b] = temp;
+        }
+
+        public int left(int i) {
+            return (2*i)+1;
+        }
+        public int right(int i) {
+            return (2*i)+2;
+        }
+        public int parent(int i) {
+            return (i-1)/2;
+        }
+    }
+    
+    public static void main(String[] args) {
+        Node n = new Node(5);
+        n.insert(10);
+        n.insert(20);
+        n.insert(50);
+        n.insert(40);
+        n.insert(80);
+
+        n.printHeap();
+
+        n.smallVal(2, 5);
+
+        n.printHeap();
+    }
+
+
+}
